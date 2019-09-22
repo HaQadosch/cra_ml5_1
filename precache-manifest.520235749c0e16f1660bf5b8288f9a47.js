a5bf870fef154f0873303c4b2c9cacf6933de420
HaQadosch Berraka <haqadosch@pm.me>
2019-09-23 00:34:28 +0100

    Updates

---
+++ precache-manifest.520235749c0e16f1660bf5b8288f9a47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bba78c321c6f8a243054aeaa0797f29",
+    "url": "/cra_ml5_1/index.html"
+  },
+  {
+    "revision": "196c231e4d58014d739c",
+    "url": "/cra_ml5_1/static/css/main.3a06c2f2.chunk.css"
+  },
+  {
+    "revision": "707c14a5b15ab113314f",
+    "url": "/cra_ml5_1/static/js/2.99c0c332.chunk.js"
+  },
+  {
+    "revision": "196c231e4d58014d739c",
+    "url": "/cra_ml5_1/static/js/main.99455975.chunk.js"
+  },
+  {
+    "revision": "bbcabedc88f5c960772c",
+    "url": "/cra_ml5_1/static/js/runtime-main.8ac98872.js"
+  }
+]);
